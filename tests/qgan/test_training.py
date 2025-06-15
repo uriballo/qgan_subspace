d64b4f0d54969c9ecc239a2aec32d24219d464e2
Guillermo Abad López <109400222+GuillermoAbadLopez@users.noreply.github.com>
2025-06-15 18:45:54 +0200

    Improve integration test, and merge them to pytest

--- tests/qgan/test_training.py
+++ tests/qgan/test_training.py
@@ -1,5 +1,8 @@
 import sys
 import os
+
+from tools.data.data_managers import print_and_train_log
+from tools.training_init import run_test_configurations
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
 
 from config import CFG
@@ -14,5 +17,10 @@ class TestTraining():
         assert t.total_target_state is not None
 
     def test_training_run(self):
-        # No need for this, since I'll do that with running main_testing.py
-        ...
+        # Change results directory to TESTING:
+        CFG.base_data_path = f"./generated_data/TESTING/{CFG.run_timestamp}"
+        CFG.set_results_paths()
+
+        # Run the test configurations:
+        print_and_train_log("Running in TESTING mode.\n", CFG.log_path)
+        run_test_configurations()
