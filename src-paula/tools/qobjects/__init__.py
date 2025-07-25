c2c06de0cb04eb2ef61c1f6edd596d9e96f313ff
PaulaGarciaMolina <paula.garciamolina@estudiante.uam.es>
2025-07-25 12:44:43 +0200

    Remove old versions

--- src-paula/tools/qobjects/__init__.py
+++
@@ -1,4 +0,0 @@
-# This file marks the qobjects directory as a Python package.
-
-from .qcircuit import QuantumCircuit
-from .qgates import Identity, QuantumGate
--- src_sparse/tools/data/__init__.py
+++
@@ -1,1 +0,0 @@
-# This file marks the data directory as a Python package.
